1ca0171137262c2e53e7ab05e448369992ad3cc2
Jessie Wei <jessieweiyi@gmail.com>
2023-06-06 14:40:57 +1000

    chore: Remove the global flag in the regex match

--- packages/threat-composer/src/configs/content.ts
+++ packages/threat-composer/src/configs/content.ts
@@ -13,4 +13,4 @@
   See the License for the specific language governing permissions and
   limitations under the License.
  ******************************************************************************************************************** */
-export const REGEX_CONTENT_NOT_HTML_TAG = /(<.*?>)/gi;
+export const REGEX_CONTENT_NOT_HTML_TAG = /(<.*?>)/i;
