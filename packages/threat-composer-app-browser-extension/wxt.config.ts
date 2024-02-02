c105dd730564eeefcd6afbdcd8e853cd5f53344b
Darran Boyd <dboyd13@gmail.com>
2024-02-02 18:37:17 +1100

    chore(Browser extension): Improve build scripts (#75)
    Co-authored-by: Darran Boyd <drboyd@amazon.com>

--- packages/threat-composer-app-browser-extension/wxt.config.ts
+++ packages/threat-composer-app-browser-extension/wxt.config.ts
@@ -1,35 +1,51 @@
-import { defineConfig, TargetManifestVersion } from 'wxt';
+import { defineConfig, type UserManifest } from 'wxt';
 import react from '@vitejs/plugin-react';
 import copy from 'rollup-plugin-copy';
 
 const tcScriptInjectForThreatComposer = 'scriptInjectForThreatComposer.js'
 
-const baseManifest = {
-  name: 'Threat Composer Viewer',
-  description: "View a Threat Composer JSON export in Threat Composer",
-  version_name: "0.0.3-alpha",
-  content_scripts: [
+function generateManifest(env: ConfigEnv): UserManifest {
+
+  const manifest: UserManifest = {
+    name: 'Threat Composer Viewer',
+    description: "View a Threat Composer JSON export in Threat Composer",
+    version_name: "0.0.3-alpha",
+    content_scripts: [
+      {
+        matches: ["*://*/*.tc.json*", "*://*.github.com/*"],
+        js: ['content-script.js'],
+        run_at: "document_end"
+      }
+    ],
+    permissions: ["storage", "tabs"],
+    icons: {
+      128: '/icon-128.png',
+    },
+  }
+
+  if (env.manifestVersion === 2) {
+    manifest.permissions?.push("*://*.github.com/*")
+    manifest.permissions?.push("*://code.amazon.com/*")
+  }
+
+  const webAccessibleResources = [
     {
-      matches: ["*://*/*.tc.json*", "*://*.github.com/*"],
-      js: ['content-script.js'],
-      run_at: "document_end"
+      "resources": ["scriptInjectForCodeCatalyst.js"],
+      "matches": ["https://codecatalyst.aws/*"]
     }
-  ],
-  permissions: ["storage", "tabs", "*://*.github.com/*", "*://code.amazon.com/*"],
-  icons: {
-    128: '/icon-128.png',
-  },
+  ];
+
+  if (env.browser === 'firefox') {
+    manifest.web_accessible_resources = webAccessibleResources.flatMap(entry => entry.resources)
+  } else {
+    manifest.web_accessible_resources = webAccessibleResources
+  }
+  return manifest
+
 }
 
-const webAccessibleResources = [
-  {
-    "resources": ["scriptInjectForCodeCatalyst.js"],
-    "matches": ["https://codecatalyst.aws/*"]
-  }
-];
-
 export default defineConfig({
-  vite: () => ({
+  vite: (env) => ({
     build: {
       emptyOutDir: false
     },
@@ -38,25 +54,20 @@ export default defineConfig({
       copy({
         targets: [{
           src: '../threat-composer-app/build/browser-extension/index.html',
-          dest: ['./.output/chrome-mv3', './.output/firefox-mv2'],
-          transform: (contents, filename) => contents.toString().replace('<\/body><\/html>', '<script src=\"' + tcScriptInjectForThreatComposer + '\"><\/script><\/body><\/html>')
+          dest: env.browser === 'chrome' ? ['./.output/chrome-mv3'] : ['./.output/firefox-mv2'],
+          transform: (contents) => contents.toString().replace('<\/body><\/html>', '<script src=\"' + tcScriptInjectForThreatComposer + '\"><\/script><\/body><\/html>')
         }],
         copyOnce: true
       }),
       copy({
         targets: [{
           src: ['../threat-composer-app/build/browser-extension/**/*', '!../threat-composer-app/build/browser-extension/index.html'],
-          dest: ['./.output/chrome-mv3', './.output/firefox-mv2'],
+          dest: env.browser === 'chrome' ? ['./.output/chrome-mv3'] : ['./.output/firefox-mv2'],
         }],
         copyOnce: true
       })
     ]
   }),
   srcDir: 'src',
-  manifest: ({ manifestVersion }) => ({
-    ...baseManifest,
-    web_accessible_resources: manifestVersion === 3
-      ? webAccessibleResources
-      : webAccessibleResources.flatMap(entry => entry.resources),
-  })
+  manifest: generateManifest
 });
